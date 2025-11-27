d4345f34a4dd7677d3096090e1b6a013f91b9ed2
gomezdaniela0705-tech <gomezdaniela0705@gmail.com>
2025-11-27 10:59:50 -0500

    lista.c

--- array_list.h
+++ array_list.h
@@ -1,19 +1,14 @@
-#include <stdio.h>
+#ifndef ARRAY_LIST_H
+#define ARRAY_LIST_H
 
-#ifndef ARAY_LIST
-#define ARRRAY_LIST
-
-typedef struc{
+typedef struct{
 	void **data; //Aqui se almacenan los elementos genéricos
 	int size; //Numero de elemntos que tiene la lista almacenados
 	int capacity; //Capacidad maxima de objetos que tiene la lista
 	int element_size; //Tamaño en byts de cada elemento
 }ArrayList;
 
-//Constructor y destructor
-//Constructor-> Inicializa variables en sus valores por efecto y pide memoria para almacenar
-//Destructor->Realiza las liberaciones de momoria par que el 05 puede volver a usarla
-
+//crear o borrar lista
 ArrayList* arrayList_create(int initia_capacity, int element_size);
 void arrayList_destroy(ArrayList *list);
 
@@ -25,7 +20,7 @@ int arraylist_remove(ArrayList *list, int index);//Se quita el elemnto en la pos
 void arraylist_clear(ArrayList *list);//Se borra todos los elemtos de la lista
 
 //Funciones usadas para manipular los arreglos dinamicos
-void arraylist_ensure_capacity(ArrayList *list, int min_capacity);//Pide memoria si esta llen
+void arraylist_ensure_capacity(ArrayList *list, int min_capacity);//Pide mas memoria si se esta llenando
 void arratlist_trim_to_size(ArrayList *list);//Ajusta el tamaño dinamico al usuariompor los elementos
 ArrayList* arraylist_clone(Array *list);//Nos sirve para copiar una lista a otra
 
