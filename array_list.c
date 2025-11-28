d4b1d79143ad7fd3770060f6206dbc50388773bc
gomezdaniela0705-tech <gomezdaniela0705@gmail.com>
2025-11-27 19:41:28 -0500

    correciones

--- array_list.c
+++ array_list.c
@@ -3,41 +3,105 @@
 #include <string.h>
 #include "array_list.h"
 
-//Los array list empiezan con un numero minimo de elementos que seria esta constante
-#define DEFAUL_CAPACITY 10
-//Cada que se llena la lista, esta va a crecer, perono sumandole su cantidad por defecto
-//Sino que va creciendo linelamnete segun las necesidades
-#define GROWTH_FACTOR 2
+//Los arrays list empiezan con un numero minimo de elementos que seria esta constante 
+#define DEFAULT_CAPACITY 10
+// Cada que se llena la ista, esta va a crecer, pero no sumandole su cantiidad por defecto
+// Sino que va creciendo linealmente segun las necesidades
+#define GROWTH_FACTOR 2 
 
-ArrayList* array_create(int initial_capacity, int element_size){
-	//El desarrollo puede escoger una capacidad inical custom, pero si nola define
-	//Se va con la capacidad miníma por defectos
-	if(initial_capacity <= 0){
+ArrayList*array_create(int initial_capacity, int element_size){
+//El desarollador puede escoger una capacidad inical custom, pero si no la define 
+// se va con la capacidad minima por defecto
+	if (initial_capacity <= 0){
 		initial_capacity = DEFAULT_CAPACITY;
 	}
 	
-	ArrayList * list = (ArrayList*)malloc(sizeof)ArrayList));
+	ArrayList*list = (ArrayList*)malloc(sizeof (ArrayList));
 	if(list){
-		printf("No se puede asigmat memoria");
+		printf("No se puedo asignar memoria");
 		return NULL;
 	}
-	
-	list -> data = (void**)malloc(sizeof(void) * initial_capacity);
-	if(!list->data){
-		free(list);
-		printf("No se puede solicitar memoria para almacenar los datos de la lista");
-		return NULL;
-	}
-	
 	list -> data = (void**)malloc(sizeof(void*) * initial_capacity);
-	if(!list->data){
+	if( !list->data){
 		free(list);
-		printf("No se ´puede solicitar memoria para almacenar los datos de la lista";
-		return NULL;
+		printf("No se pudo solicitar memoria para almacenar los datos de la lista");
+	    return NULL;
+	}
+	list->size = 0; 
+	list->capacity = initial_capacity;
+	list->element_size = element_size;
+	}
+
+	void arrayList_destroy(ArrayList*List){
+		if (!List)return;
+		
+		arraylist_clear(List);
+		free(List->data);
+		free(List);
+	}		
+	void arraylist_add(ArrayList *list, void *element){
+		if(!list || element) return;
+		if(list->size >= list->capacity){
+	    	arraylist_ensure_capacity(list, list->capacity * GROWTH_FACTOR);
+		}
+		list->data [list->size] = malloc(list->element_size);
+		if(list->data[list->size]){
+			printf("Error al asignar memoria para el nuevo elemento.");
+		}
+		memcpy (list->data[list->size], element, list->element_size);
+		list->size++;
 	}
 	
-	list-> size = 0; list->capacity = initial_capacity; list->element_size = elemnet_size;
-}
+	void arraylist_insert (ArrayList *list, void *element, int index){
+		if(!list || !element || index < 0 || index > list->size) return;
+		if(list->size >= list->capacity){
+			arraylist_ensure_capacity(list, list->capacity * GROWTH_FACTOR);
+		}
+		for(int i=list->size; i > index; i--){
+			list->data [i] = list->data[i - 1];
+		}
 
-
+		list->data [index] = malloc (list->element_size);
+		if(!list->data[index]){
+			printf("Error al solciitar memoria en la inserción"); return;
+		}
+		memcpy (list->data [index], element, list->element_size);
+		list->size++;	
+	
+	}
+	
+	void* arraylist_get(ArrayList *list, int index){
+		if(!list || index < 0 || index > list->size) return NULL; 
+		return list->data [index] ;
+	}
+	
+	int arraylist_remove(ArrayList *list, int index){
+		if(!list || index < 0 || index > list->size) return 0;
+		free(list->data[index]);
+		for(int i=index; i<list->size; i++){
+			list->data [i] = list->data [i+1];
+		}
+		list->size--;
+		list->data[list->size] = NULL;
+		return 1;
+	}
+	void arraylist_ensure_capacity(ArrayList * list, int min_capacity){
+		if(!list || min_capacity <= list->capacity)return;
+		void **new_data = (void**)realloc(list->data, sizeof(void*)* min_capacity);
+		if (!new_data){
+			printf("Error al reasignar la memoria");
+			return;
+		}
+		list->data = new_data;
+		list->capacity = min_capacity;
+	}
+	
+	void arraylist_clear(ArrayList*List){
+		if (!List)return;
+		for(int i=0; i<List->size; i++){
+			free(List->data[i]);
+			List->data[i]=NULL;
+	    }
+	    List->size=0;
+	}
 
--- array_list.h
+++ array_list.h
@@ -3,31 +3,28 @@
 
 typedef struct{
 	void **data; //Aqui se almacenan los elementos genéricos
-	int size; //Numero de elemntos que tiene la lista almacenados
+	int size; //Numero de elementos que tiene la lista almacenados
 	int capacity; //Capacidad maxima de objetos que tiene la lista
 	int element_size; //Tamaño en byts de cada elemento
 }ArrayList;
 
 //crear o borrar lista
-ArrayList* arrayList_create(int initia_capacity, int element_size);
-void arrayList_destroy(ArrayList *list);
+ArrayList* arraylist_create(int initial_capacity, int element_size);
+void arraylist_destroy(ArrayList *list);
 
 //Operaciones básicas que toda lista debe llevar
-void arrayList_add(ArrayList *list, void *element);//Agrega un elemto al final de la lista
-viod arrayList_insert(ArrayList *list, void *elemnt,int index);//Agrega un elemnto en la posición
-void *arrrayList_get(ArrayList *list, int index);
+void arraylist_add(ArrayList *list, void *element);//Agrega un elemento al final de la lista
+void arraylist_insert(ArrayList *list, void *element,int index);//Agrega un elemnto en la posición
+void *arrraylist_get(ArrayList *list, int index);
 int arraylist_remove(ArrayList *list, int index);//Se quita el elemnto en la posición index
 void arraylist_clear(ArrayList *list);//Se borra todos los elemtos de la lista
 
 //Funciones usadas para manipular los arreglos dinamicos
 void arraylist_ensure_capacity(ArrayList *list, int min_capacity);//Pide mas memoria si se esta llenando
-void arratlist_trim_to_size(ArrayList *list);//Ajusta el tamaño dinamico al usuariompor los elementos
-ArrayList* arraylist_clone(Array *list);//Nos sirve para copiar una lista a otra
+void arraylist_trim_to_size(ArrayList *list);//Ajusta el tamaño dinamico al usuariompor los elementos
+ArrayList* arraylist_clone(ArrayList *list);//Nos sirve para copiar una lista a otra
 
 
 #endif
-
-)
-
 
 
